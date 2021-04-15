e08aec41f299ea0a94acf9e88d080bf9e81722f2
Santiago Morales <santiago.is.afk@gmail.com>
2021-04-15 15:35:30 -0500

    finished project upload

---
+++ Assets/GameUI.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    public SphereMov _sphereMov;
+    public GameObject panel1, panel2;
+    public Slider sldAngle, sldVelocity, sldEstimate;
+    public Text txtAngle, txtVelocity, txtEstimate;
+    public Text txtResult;
+    public LineRenderer lineaEstimate, lineaReal;
+    
+    
+    public GameObject estimateGO;
+    
+    public void Reset()
+    {
+        panel1.SetActive(true);
+        panel2.SetActive(false);
+        
+        txtResult.text = "";
+        
+        txtAngle.text = "Angulo inicial: "+sldAngle.value.ToString();
+        txtVelocity.text = "Velocidad inicial: "+sldVelocity.value.ToString();
+        txtEstimate.text = "Distancia estimada: "+sldEstimate.value.ToString();
+        
+        _sphereMov.ResetSimulation();
+        lineaReal.SetPosition(1, Vector3.zero);
+        
+        StopAllCoroutines();
+    }
+    
+    public void UpdateData()
+    {
+        _sphereMov.angle = sldAngle.value;
+        _sphereMov.v0 = sldVelocity.value;
+        
+        
+        txtAngle.text = "Angulo inicial: "+sldAngle.value.ToString();
+        txtVelocity.text = "Velocidad inicial: "+sldVelocity.value.ToString();
+        txtEstimate.text = "Distancia estimada: "+sldEstimate.value.ToString();
+        
+        lineaEstimate.SetPosition(1, new Vector3(sldEstimate.value,0,0));
+        estimateGO.transform.localPosition = new Vector3(sldEstimate.value, estimateGO.transform.localPosition.y, 0);
+    }
+
+    public void StartRealTimeSimulation()
+    {
+        panel1.SetActive(false);
+        panel2.SetActive(true);
+        StartCoroutine(_sphereMov.Simulate(sldEstimate.value, true, (result) =>
+        {
+            ShowResult(result);
+        })  );
+    }
+    
+    public void StartSimulation()
+    {
+        panel1.SetActive(false);
+        panel2.SetActive(true);
+        StartCoroutine(_sphereMov.Simulate(sldEstimate.value, false, (result) =>
+        {
+            ShowResult(result);
+        })  );
+    }
+
+    public void ShowResult(bool result)
+    {        
+        if (result)
+        {
+            txtResult.text = $"Has acertado, la distancia real es de {_sphereMov.x}";
+        }
+        else
+        {
+            txtResult.text = $"Has fallado, la distancia real es de {_sphereMov.x}";
+        }           
+            
+        lineaReal.SetPosition(1, new Vector3(_sphereMov.x,0,0));
+    }
+}
---
+++ Assets/SphereMov.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMov : MonoBehaviour
+{
+    public float x;
+    public float v0, angle;
+    private float g = 9.8f;
+    private bool withoutStart = true;
+
+    
+    public void ResetSimulation()
+    {
+        withoutStart = true;
+        this.transform.localPosition = Vector3.zero;
+    }
+    
+    public IEnumerator Simulate(float estimate, bool realTime, Action<Boolean> result)
+    {
+        float time = 0;
+        while (this.transform.localPosition.y > 0 || withoutStart)
+        {
+            if (realTime) time += Time.deltaTime;
+            else time += 0.01f;
+            
+            this.transform.localPosition = new Vector3(v0*Mathf.Cos((angle*Mathf.PI)/180)*time, v0*Mathf.Sin((angle*Mathf.PI)/180)*time-(0.5f*g*time*time),0);
+            withoutStart = false;
+            
+            if (realTime) yield return null;
+            else yield return new WaitForSeconds(0.01f);
+        }
+
+        x = (v0*v0*Mathf.Sin((angle*Mathf.PI)/90))/g; 
+        
+        this.transform.localPosition = new Vector3(x, 0,0);
+        
+        bool _result = Mathf.Abs(estimate - (x)) <= x*0.05f ? true: false;
+        result(_result);
+    }
+}
